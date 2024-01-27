5594b042eed4aac01e0d64eef5120e4cb4681c0b
mst-mkt <s1f102303057@iniad.org>
2024-01-28 00:43:25 +0900

    fix: remove useless implementations on the top page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-import { spotifyClient } from '@/service/spotifyClient'
 import styles from './page.module.css'
 
 export default async function Home() {
-  const tracks = await spotifyClient.search('あ', ['track'], 'JP')
-  const devices = await spotifyClient.player.getAvailableDevices()
   return (
     <main className={styles.main}>
-      {tracks.tracks.items.map((track) => (
-        <div key={track.id}>
-          <img src={track.album.images[0].url} alt={track.name} />
-          <div>
-            <h2>{track.name}</h2>
-            <p>{track.artists.map((artist) => artist.name).join(', ')}</p>
-          </div>
-        </div>
-      ))}
+      <p>Hello, Hackathon</p>
     </main>
   )
 }
