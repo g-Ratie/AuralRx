ece06fd7e4e77968c59e066b376879c41bc5c48a
1F10220146 <s1f102201469@iniad.org>
2024-01-30 19:05:13 +0900

    Refactor: getGoogleAccessToken function

--- src/service/getGoogleAccessToken.ts
+++ src/service/getGoogleAccessToken.ts
@@ -3,14 +3,7 @@ import { authOptions } from './nextAuthConfig'
 
 export const getGoogleAccessToken = async () => {
   const session = await getServerSession(authOptions)
-  if (session?.provider !== 'google') {
-    return null
-  }
-  if (!session) {
-    return null
-  }
-  if (!session.user.accessToken) {
-    return null
-  }
-  return session.user.accessToken
+  return session && session.provider === 'google' && session.user.accessToken
+    ? session.user.accessToken
+    : null
 }
