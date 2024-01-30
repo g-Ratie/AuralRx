30b0383a7db58bf7b5fae600b926423eb194648e
1F10220146 <s1f102201469@iniad.org>
2024-01-30 19:02:27 +0900

    Refactor: Update access token retrieval in fitness routes

--- src/app/api/fitness/route.ts
+++ src/app/api/fitness/route.ts
@@ -1,13 +1,13 @@
 import { getFitnessData } from '@/common/fitness/googleFitService'
-import { getaccessToken } from '@/service/checkAccesstoken'
+import { getGoogleAccessToken } from '@/service/getGoogleAccessToken'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  const accessTokenOrError = await getaccessToken()
-  if (accessTokenOrError instanceof NextResponse) {
-    return accessTokenOrError
+  const googleAccessToken = await getGoogleAccessToken()
+  if (googleAccessToken === null) {
+    return NextResponse.json({ error: 'No access token' }, { status: 401 })
   }
 
-  const res = NextResponse.json(await getFitnessData(accessTokenOrError))
+  const res = NextResponse.json(await getFitnessData(googleAccessToken))
   return res
 }
