85a8f65a8a26e8b388dd34103da39eb7b7d18f89
mst-mkt <s1f102303057@iniad.org>
2024-01-31 01:03:54 +0900

    refactor: move and organize component (`_components/Login.tsx` -> `page.tsx`)

--- src/app/auth/google/page.tsx
+++ src/app/auth/google/page.tsx
@@ -1,14 +1,23 @@
 import { authOptions } from '@/service/nextAuthConfig'
 import { getServerSession } from 'next-auth'
+import { getProviders } from 'next-auth/react'
+import { LoginButton } from '../_components/LoginButton'
 import { Fitness } from './_components/Fitness'
-import { Login } from './_components/Login'
+import styles from './page.module.css'
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
 
-  if (session?.provider !== 'google') return <Login />
+  if (session?.provider === 'google') return <Fitness />
 
-  return <Fitness />
+  const provider = await getProviders()
+
+  return (
+    <div className={styles.container}>
+      <p>Google連携</p>
+      {provider?.google && <LoginButton loginProvider={provider.google} />}
+    </div>
+  )
 }
 
 export default Home
