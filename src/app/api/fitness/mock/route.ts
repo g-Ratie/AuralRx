ca636929611f9d2d0b75f7edce5c76131c5c5ed9
1F10220146 <s1f102201469@iniad.org>
2024-02-01 02:52:08 +0900

    fix: change return data

--- src/app/api/fitness/mock/route.ts
+++ src/app/api/fitness/mock/route.ts
@@ -2,6 +2,17 @@ import { chatUtils } from '@/common/LLM/chatUtils'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(req: NextRequest) {
-  const res = NextResponse.json(await chatUtils.analyzeHealthData())
+  const analyzeResult = await chatUtils.analyzeHealthData()
+  const recommendParams = await Promise.all(
+    analyzeResult.activity_analysis.map(async (activity) => {
+      const recommendResult = await chatUtils.recommendSongParameter(
+        JSON.stringify(activity.activity_inference),
+        null,
+      )
+      return recommendResult
+    }),
+  )
+
+  const res = NextResponse.json({ analyzeResult, recommendParams: recommendParams })
   return res
 }
