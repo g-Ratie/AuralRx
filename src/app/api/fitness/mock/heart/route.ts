b1cb772a3ee14a355ec08049535830196229d8f5
1F10220146 <s1f102201469@iniad.org>
2024-02-01 03:48:34 +0900

    Remove: mock heart and step routes

--- src/app/api/fitness/mock/heart/route.ts
+++
@@ -1,7 +0,0 @@
-import { getMockHeartRate } from '@/common/fitness/googleFitMock'
-import { NextRequest, NextResponse } from 'next/server'
-
-export async function GET(req: NextRequest) {
-  const res = NextResponse.json(await getMockHeartRate())
-  return res
-}
