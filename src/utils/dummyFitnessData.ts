68494abad8e33dac421e678506d9c57e2a84f8bd
1F10220146 <s1f102201469@iniad.org>
2024-02-01 22:24:15 +0900

    feat: Add dummy fitness data for chart debugging

---
+++ src/utils/dummyFitnessData.ts
@@ -0,0 +1,134 @@
+//チャートのデバッグ用に調整したダミーデータ
+export const sampleFitnessData = {
+  analyzeResult: {
+    activity_analysis: [
+      {
+        start_time: '08:00',
+        end_time: '09:00',
+        heart_rate_summary: {
+          average: 100,
+          min: 98.25,
+          max: 100.75,
+        },
+        steps_summary: {
+          total: 10142,
+        },
+        activity_inference:
+          'この時間帯は、歩数が多いことから、ウォーキングやランニングなどの運動をしていた可能性があります。',
+      },
+      {
+        start_time: '09:00',
+        end_time: '10:00',
+        heart_rate_summary: {
+          average: 110,
+          min: 98.25,
+          max: 118,
+        },
+        steps_summary: {
+          total: 6746,
+        },
+        activity_inference:
+          'この時間帯は、心拍数がやや高めであることから、デスクワークや軽い運動をしていた可能性があります。',
+      },
+      {
+        start_time: '10:00',
+        end_time: '11:00',
+        heart_rate_summary: {
+          average: 100,
+          min: 98.25,
+          max: 113.75,
+        },
+        steps_summary: {
+          total: 7705,
+        },
+        activity_inference:
+          'この時間帯は、歩数が多いことから、ウォーキングやランニングなどの運動をしていた可能性があります。',
+      },
+      {
+        start_time: '11:00',
+        end_time: '12:00',
+        heart_rate_summary: {
+          average: 88,
+          min: 82,
+          max: 113.75,
+        },
+        steps_summary: {
+          total: 2070,
+        },
+        activity_inference:
+          'この時間帯は、心拍数が低めで歩数も少ないことから、休憩していた可能性があります。',
+      },
+      {
+        start_time: '12:00',
+        end_time: '13:00',
+        heart_rate_summary: {
+          average: 106,
+          min: 82,
+          max: 113.75,
+        },
+        steps_summary: {
+          total: 4117,
+        },
+        activity_inference:
+          'この時間帯は、心拍数がやや高めで歩数もそこそこあることから、軽い運動をしていた可能性があります。',
+      },
+      {
+        start_time: '13:00',
+        end_time: '14:00',
+        heart_rate_summary: {
+          average: 109,
+          min: 104.25,
+          max: 113.75,
+        },
+        steps_summary: {
+          total: 6545,
+        },
+        activity_inference:
+          'この時間帯は、歩数が多いことから、ウォーキングやランニングなどの運動をしていた可能性があります。',
+      },
+      {
+        start_time: '14:00',
+        end_time: '15:00',
+        heart_rate_summary: {
+          average: 88,
+          min: 73.75,
+          max: 104.25,
+        },
+        steps_summary: {
+          total: 3064,
+        },
+        activity_inference:
+          'この時間帯は、心拍数が低めで歩数も少ないことから、休憩していた可能性があります。',
+      },
+      {
+        start_time: '15:00',
+        end_time: '16:00',
+        heart_rate_summary: {
+          average: 150,
+          min: 93.5,
+          max: 10605,
+        },
+        steps_summary: {
+          total: 10605,
+        },
+        activity_inference:
+          'この時間帯は、歩数が多いことから、ウォーキングやランニングなどの運動をしていた可能性があります。',
+      },
+      {
+        start_time: '16:00',
+        end_time: '17:00',
+        heart_rate_summary: {
+          average: 100,
+          min: 93.5,
+          max: 102.5,
+        },
+        steps_summary: {
+          total: 5753,
+        },
+        activity_inference:
+          'この時間帯は、歩数が多いことから、ウォーキングやランニングなどの運動をしていた可能性があります。',
+      },
+    ],
+  },
+  recommendParams: [],
+}
