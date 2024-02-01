a920dba5afc808192637666c4f1c4e2e0e78f959
1F10220146 <s1f102201469@iniad.org>
2024-02-02 07:43:30 +0900

    Refactor: FetchDataButton component to use API endpoint for fetching recommended data

--- src/app/chart/_components/FetchDataButton.tsx
+++ src/app/chart/_components/FetchDataButton.tsx
@@ -1,11 +1,37 @@
 'use client'
+import { PostFitnessAPIRequestBody } from '@/app/api/fitness/route'
 import { FitnessOutput } from '@/common/LLM/fitnessOutputSchema'
+import { extendedRecommendationSchema } from '@/common/LLM/recommendOutputSchema'
+import { Button } from '@/components/ui/Button'
 import { fetchRecommendedDataWithMock } from '@/utils/fetchAPIData'
 
 export default function FetchDataButton({ fitnessData }: { fitnessData: FitnessOutput }) {
+  async function fetchRecommendedData(fitnessData: FitnessOutput, seedTrack: string[] | null) {
+    try {
+      const requestBody: PostFitnessAPIRequestBody = {
+        activityAnalysis: fitnessData.activityAnalysis,
+        seedTrack: seedTrack,
+      }
+      const response = await fetch('http://localhost:3000/api/fitness', {
+        method: 'POST',
+        body: JSON.stringify(requestBody),
+        headers: { 'Content-Type': 'application/json' },
+      })
+      if (!response.ok) {
+        throw new Error('Network response was not ok')
+      }
+      const responseData = await response.json()
+      const parsedRecommendedData = responseData.map((item: unknown) =>
+        extendedRecommendationSchema.parse(item),
+      )
+      return parsedRecommendedData
+    } catch (error) {
+      return null
+    }
+  }
   const handleClick = async () => {
     try {
-      const recommendedData = await fetchRecommendedDataWithMock(fitnessData, null)
+      const recommendedData = await fetchRecommendedData(fitnessData, null)
       console.log('Recommended Data:', recommendedData)
     } catch (error) {
       console.error('Error fetching recommended data:', error)
@@ -14,6 +40,6 @@ export default function FetchDataButton({ fitnessData }: { fitnessData: FitnessO
 
   return (
     // biome-ignore lint/a11y/useButtonType: <explanation>
-    <button onClick={handleClick}>Fetch recommended data</button>
+    <Button onClick={handleClick} label="レコメンドを取得" />
   )
 }
