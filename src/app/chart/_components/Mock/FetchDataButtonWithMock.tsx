b2d789924e7df8b0b7af5468ae1a32a675cf3951
1F10220146 <s1f102201469@iniad.org>
2024-02-03 00:35:34 +0900

    feat: Add API calls to fetch recommended songs and create a playlist

--- src/app/chart/_components/Mock/FetchDataButtonWithMock.tsx
+++ src/app/chart/_components/Mock/FetchDataButtonWithMock.tsx
@@ -6,14 +6,30 @@ import { fetchRecommendedDataWithMock } from '@/utils/fetchAPIData'
 const FetchDataButtonWithMock = ({ fitnessData }: { fitnessData: FitnessOutput }) => {
   const handleClick = async () => {
     try {
-      const recommendedData = await fetchRecommendedDataWithMock(fitnessData, null)
-      console.log('Recommended Data:', recommendedData)
+      const recommendedParams = await fetchRecommendedDataWithMock(fitnessData, null)
+      const recommendSongResponse = await fetch('/api/spotify/recommend', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recommendedParams),
+      })
+      const recommendSongData = await recommendSongResponse.json()
+      console.log('recommendSongData:', recommendSongData)
+
+      await fetch('/api/spotify/playlist', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(recommendSongData),
+      })
     } catch (error) {
       console.error('Error fetching recommended data:', error)
     }
   }
 
-  return <Button onClick={handleClick} label="レコメンドを取得" />
+  return <Button onClick={handleClick} label="レコメンドを取得(モック)" />
 }
 
 export default FetchDataButtonWithMock
