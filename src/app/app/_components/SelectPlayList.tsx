c60473f935d61b513e3199afba13c0ceb42f033a
mst-mkt <s1f102303057@iniad.org>
2024-02-02 22:30:54 +0900

    fix: display loading spinner when fetching playlist from spotify

--- src/app/app/_components/SelectPlayList.tsx
+++ src/app/app/_components/SelectPlayList.tsx
@@ -2,6 +2,7 @@
 
 import { PlaylistCard } from '@/components/music/PlaylistCard'
 import { Button } from '@/components/ui/Button'
+import { Loading } from '@/components/ui/Loading'
 import { SimplifiedPlaylist } from '@spotify/web-api-ts-sdk'
 import { IconMusicPlus } from '@tabler/icons-react'
 import Link from 'next/link'
@@ -15,12 +16,18 @@ type Props = {
 
 export const SelectPlayList = ({ selectedId, handleSelect }: Props) => {
   const [playlists, setPlaylists] = useState<SimplifiedPlaylist[]>([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch('/api/spotify/playlist')
       .then((res) => res.json())
       .then((data) => setPlaylists(data.playlists))
+      .finally(() => setLoading(false))
   }, [])
+
+  if (loading) {
+    return <Loading visible={loading} />
+  }
 
   return playlists.length === 0 ? (
     <div className={styles.noPlaylist}>
